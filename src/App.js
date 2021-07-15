3b271c8606e6fd8c81191c08985f45f9e90adc4e
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2021-07-15 23:50:14 +0400

    unfinished Counter-App:inputs are missing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "./components/button.jsx";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      };
+    }
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  resetCount = () => {
+    this.setState({
+      count: this.state.count = 0
+    });
+  };
+ render() {
+    let { count } = this.state;
+    return (
+      <div className="app">
+        <div className="wrapper">
+          <div class="count">
+            <h3 className="header">Count:</h3>
+            <h1 className="header">{count}</h1>
+          </div>
+          <div class="buttons">
+            <Button title={"-"} action={this.decrementCount} />
+            <Button title={"+"} action={this.incrementCount} />
+            <Button title={"Reset "} action={this.resetCount}/>
+          </div>
+          {/* <label htmlFor="new">
+           Select min number
+          </label>
+          <input
+            id="max number"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+
+          <Button title={"V"} action={this.decrementCount}/>
+          <label htmlFor="new">
+           Select max number
+          </label>
+          <input
+            id="max number"
+            onChange={this.handleChange}
+            value={this.state.number}
+          />
+          <Button title={"V"} action={this.decrementCount}/>
+
+
+          <label htmlFor="new">
+           Select step
+          </label>
+          <input
+            id="min number"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <Button title={"V"} action={this.decrementCount}/> */}
+        </div>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
